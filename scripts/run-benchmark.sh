a5ceed82bd6a7317f1f74a5998694253661e3a2a
twobiers <22715034+twobiers@users.noreply.github.com>
2023-04-15 18:23:59 +0200

    log as csv

--- scripts/run-benchmark.sh
+++ scripts/run-benchmark.sh
@@ -8,7 +8,8 @@ RESULTS_DIR="results"
 export PRODUCTS_FILE="product_ids_10k.json"
 
 current_date() {
-    date +"%Y-%m-%d_%H-%M-%S"
+    # date +"%Y-%m-%d_%H-%M-%S"
+    date +%s
 }
 
 [ ! -d "$RESULTS_DIR/$VARIANT" ] && mkdir -p "$RESULTS_DIR/$VARIANT"
@@ -21,8 +22,8 @@ snmpset -v1 -c private 192.168.178.78 1.3.6.1.4.1.28507.43.1.5.1.2.1.13.1 u 0
 
 k6 run \
     -o experimental-prometheus-rw \
-    -o "csv=$RESULTS_DIR/$VARIANT/$START_INSTANT.csv" \
-    -o "json=$RESULTS_DIR/$VARIANT/$START_INSTANT.json" \
+    -o "csv=$RESULTS_DIR/$VARIANT/${START_INSTANT}_k6.csv" \
+    -o "json=$RESULTS_DIR/$VARIANT/${START_INSTANT}_k6.json" \
     --vus $VUS \
     --duration $DURATION \
     $BENCHMARK_SCRIPT
@@ -36,4 +37,7 @@ ENERGY_USAGE=$(snmpget -Oqv -v1 -c private 192.168.178.78 1.3.6.1.4.1.28507.43.1
 END_INSTANT=$(current_date)
 echo "[$END_INSTANT] Benchmark Ended"
 
-echo "Start: $START_INSTANT, END: $END_INSTANT, VUS: $VUS, DURATION: $DURATION, VARIANT: $VARIANT, SCRIPT: $BENCHMARK_SCRIPT, ENERGY: $ENERGY_USAGE" >>$RESULTS_DIR/benchmark-log.txt
+if [ ! -f "$RESULTS_DIR/benchmark-log.txt" ]; then
+    echo "Start, End, VUS, Duration, Variant, Script, Energy" >$RESULTS_DIR/benchmark-log.csv
+fi
+echo "$START_INSTANT, $END_INSTANT, $VUS, $DURATION, $VARIANT, $BENCHMARK_SCRIPT, $ENERGY_USAGE" >>$RESULTS_DIR/benchmark-log.csv
