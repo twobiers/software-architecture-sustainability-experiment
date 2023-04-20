0218b09397986be78e889f54f17a17676072dc83
twobiers <22715034+twobiers@users.noreply.github.com>
2023-04-20 14:09:29 +0200

    fix

--- benchmark/get_iterative_product_from_id_list.js
+++ benchmark/get_iterative_product_from_id_list.js
@@ -2,14 +2,6 @@ import http from 'k6/http';
 import exec from 'k6/execution';
 import { SharedArray } from 'k6/data';
 
-export const options = {
-  scenarios: {
-    contacts: {
-      gracefulRampDown: '0s',
-    },
-  },
-};
-
 const data = new SharedArray('products', function () {
   const productsFile = __ENV.PRODUCTS_FILE || './product_ids_1k.json';
   const ids = JSON.parse(open(productsFile));
