cd07132bf9291e7555144ed1d084b19f5c93acba
twobiers <22715034+twobiers@users.noreply.github.com>
2023-04-21 23:49:09 +0200

    add netbench

---
+++ scripts/dut/run-netbench-lo.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+
+GIGABIT=1000000000
+START_BITRATE=$((1 * GIGABIT))
+END_BITRATE=$((30 * GIGABIT))
+STEP_BITRATE=$((1 * GIGABIT))
+RESULTS_DIR="results/netbench"
+
+current_date() {
+    # date +"%Y-%m-%d_%H-%M-%S"
+    date +%s
+}
+
+prepare_results_directory() {
+    mkdir -p "$RESULTS_DIR"
+    if [ ! -f "$RESULTS_DIR/iobench-log.csv" ]; then
+        echo "Start,End,Bitrate,Adapter" >"$RESULTS_DIR/netbench-log.csv"
+    fi
+}
+
+iperf3 -s &
+IPERF_SERVER_ID=$!
+
+for ((bitrate = START_BITRATE; bitrate <= END_BITRATE; bitrate += STEP_BITRATE)); do
+    echo "[$(current_date)] Running iperf3 with $bitrate"
+    START_INSTANT=$(current_date)
+    iperf3 -c localhost -b $bitrate -t 60
+    END_INSTANT=$(current_date)
+
+    echo "$START_INSTANT,$END_INSTANT,$bitrate,lo" >>"$RESULTS_DIR/netbench-log.csv"
+done
+
+kill $IPERF_SERVER_ID
