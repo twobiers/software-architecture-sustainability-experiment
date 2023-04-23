ea7b2d15e68b7ce919231b9984c8a38d06d4e60c
twobiers <22715034+twobiers@users.noreply.github.com>
2023-04-23 10:46:38 +0200

    fix

--- scripts/run-single-benchmark.sh
+++ scripts/run-single-benchmark.sh
@@ -11,8 +11,8 @@ RAMP_UP_DURATION=10s
 RAMP_DOWN_DURATION=10s
 DURATION=90s
 BENCHMARK_SCRIPT="benchmark/get_iterative_product_from_id_list.js"
-VARIANT="${VARIANT:-no-cache}-ext"
 IMAGE_TAG="${VARIANT:-no-cache}"
+VARIANT="${VARIANT:-no-cache}-ext"
 RESULTS_DIR="results"
 SLEEP_TIME=10s
 
@@ -30,7 +30,7 @@ setup_dut() {
 
     echo "[$(current_date)] Rebooting DUT"
 
-    ssh "$SSH_PARAMETER" $SSH_HOST_DUT "sudo reboot"
+    #ssh "$SSH_PARAMETER" $SSH_HOST_DUT "sudo reboot"
 
     echo "[$(current_date)] Waiting for DUT to come back online"
     sh ./scripts/waitforssh.sh $SSH_HOST_DUT
@@ -43,12 +43,10 @@ setup_dut() {
     echo "[$(current_date)] Starting Services on DUT"
 
     #Hacky way to get all services except the one we want to exclude
-    #excluded_services="XXXXXXXXXXXXXXXXXXXXXXX"
-
-    excluded_services="mongo"
+    excluded_services="XXXXXXXXXXXXXXXXXXXXXXX"
 
     if [[ ! $VARIANT == *"redis"* ]]; then
-        excluded_services="redis $excluded_services"
+        excluded_services="redis"
     fi
 
     ssh "$SSH_PARAMETER" "$SSH_HOST_DUT" "cd $DUT_EXPERIMENT_LOCATION && export VARIANT=${IMAGE_TAG} && docker compose config --services | grep -v $excluded_services | xargs docker-compose up -d"
