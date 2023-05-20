5e987794b7caa00d961fd447cd2648805886db50
twobiers <22715034+twobiers@users.noreply.github.com>
2023-05-20 14:48:03 +0200

    update extract-commands script

--- case-study-dungeon/scripts/extract_commands.py
+++ case-study-dungeon/scripts/extract_commands.py
@@ -1,16 +1,22 @@
 import glob, os, re, datetime, json, zipfile
 import dateutil.parser, time
 
-revealed_pattern = re.compile(r"\s*Key: type\s*Value: CommandDispatched")
-csv_content = "command_dispatched_count\n"
+command_dispatched_pattern = re.compile(r"\s*Key: type\s*Value: CommandDispatched")
+timestamp_pattern = re.compile(r"\s*Key: timestamp\s*Value: (.*Z)")
+event_payload_pattern = re.compile(r"(\{.*\})", re.S)
+csv_content = "timestamp,command_type\n"
 
 for file in glob.glob("logs-*.zip"):
     with zipfile.ZipFile(file, "r") as zip_ref:
         with zip_ref.open("results/logs/kaf-command.txt") as kafka_log:
             kafka_log = kafka_log.read().decode("utf-8")
             events = kafka_log.split("Headers:")
-            commands_dispatched = [event for event in events if revealed_pattern.search(event)]
-            csv_content += f"{len(commands_dispatched)}\n"
+            commands_dispatched = [event for event in events if command_dispatched_pattern.search(event)]
+            for event in commands_dispatched:
+                event_payload = json.loads(event_payload_pattern.search(event).group(1))
+                timestamp = dateutil.parser.isoparse(timestamp_pattern.search(event).group(1))
+                command_type = event_payload["commandType"]
+                csv_content += f"{int(timestamp.timestamp())},{command_type}\n"
 
 f = open("results/case-study-dungeon/commands.csv", "w")
 f.write(csv_content)
